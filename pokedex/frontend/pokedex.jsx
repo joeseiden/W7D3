9110888176e3b9c8d2d71610ce11daea688c3468
joeseiden <joe.seiden214@gmail.com>
2017-03-08 15:11:53 -0800

    Create action creator and Ajax request for fetching Pokemon

--- pokedex/frontend/pokedex.jsx
+++ pokedex/frontend/pokedex.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// import { receiveAllPokemon } from './actions/pokemon_actions';
+// import { fetchAllPokemon } from './util/api_util';
+
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
+
+
   ReactDOM.render(<h1>I am Pokedex</h1>, root);
 });
+
+// window.receiveAllPokemon = receiveAllPokemon;
+// window.fetchAllPokemon = fetchAllPokemon;
